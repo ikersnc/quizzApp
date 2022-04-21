a3295202d1dff39383e52f44541b0160b8a76f68
ikersnc <ikersanchez51@gmail.com>
2022-04-21 18:45:55 +0200

    configurar partida

--- js/play.js
+++ js/play.js
@@ -5,3 +5,11 @@ function play() {
     home.classList.add('hidden');
     form.classList.remove('hidden');
 };
+
+addEventListener('backbutton', function() {
+    let home = document.getElementById('home');
+    let form = document.getElementById('form');
+
+    home.classList.remove('hidden');
+    form.classList.add('hidden');
+});
