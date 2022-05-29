0235d110adda84b0d9a5d9764072b70d2f199688
Iker Sánchez <101869718+ikersnc@users.noreply.github.com>
2022-05-29 18:29:45 +0200

    end redirects to inde now

--- js/end.js
+++ js/end.js
@@ -25,5 +25,5 @@ saveScore.addEventListener('click', e => {
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/');
+    window.location.assign('../index.html');
 });
